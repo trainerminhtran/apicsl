940252eb49b12507a5ab3911eb769feaa3928547
Minh Tran <trainer.minhtran@gmail.com>
2018-04-05 09:41:46 +0700

    update router

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var cors = require('cors');
 var routes = require('./routes/index');  
 var users = require('./routes/users');  
 var Courses = require('./routes/Courses');  
+var UserInCourse = require('./routes/UserInCourse');  
+
 var app = express();  
 // view engine setup  
 app.set('views', path.join(__dirname, 'views'));  
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);  
 app.use('/users', users);  
 app.use('/Courses', Courses);  
+app.use('/UserInCourse', UserInCourse);  
 // catch 404 and forward to error handler  
 app.use(function(req, res, next) {  
     var err = new Error('Not Found');  
